7ff1de2013d74e8b0704995699a459647a08712a
Chieh Hung Liu (Squall Liu) <ltes50414@gmail.com>
2023-12-02 21:55:33 +0000

    Minor fix to the previous submission.
    - Revert format change for scene mip buffer. R8 is too low for it!

--- Runtime/Renderer/RendererInitializer.cpp
+++ Runtime/Renderer/RendererInitializer.cpp
@@ -71,7 +71,7 @@ UHDeferredShadingRenderer::UHDeferredShadingRenderer(UHGraphic* InGraphic, UHAss
 	RTShadowFormat = UH_FORMAT_R8_UNORM;
 
 	// mip rate format
-	SceneMipFormat = UH_FORMAT_R8_UNORM;
+	SceneMipFormat = UH_FORMAT_R16_UNORM;
 
 	for (int32_t Idx = 0; Idx < UHRenderPassMax; Idx++)
 	{
